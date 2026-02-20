5fbb9498e3b9f10c4f55aca0e5a989394c878201
Clems <79931992+BLQuatre@users.noreply.github.com>
2026-02-20 11:00:35 +0100

    feat(P1): Add Vagrant configuration and installation scripts for K3s and Kubectl

---
+++ p1/scripts/sw_install-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# OpenRC is require for K3s
+apk add openrc
+
+# Install K3s in agent mode
+wget -qO- https://get.k3s.io | INSTALL_K3S_EXEC="agent" sh -s -
+
+# Install Kubectl
+wget -q https://dl.k8s.io/release/v1.35.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
