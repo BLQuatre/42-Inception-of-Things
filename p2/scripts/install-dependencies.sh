bbfeab83c07e2dfc94c965c44b443cb5b01c5b07
MiniKlar <omontloris@gmail.com>
2026-03-30 00:56:08 +0200

    (P2): Added Vagrantfile and install-dependencies script for K3s setup

---
+++ p2/scripts/install-dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#install k3s
+curl -sfL https://get.k3s.io | sh -
+
+#Applying app one configuration
+kubectl apply -f app-one-deployment.yaml
+kubectl apply -f app-one-service.yaml
+
+#Applying app two configuration
+kubectl apply -f app-two-deployment.yaml
+kubectl apply -f app-two-service.yaml
+
+#Applying app three configuration
+kubectl apply -f app-three-deployment.yaml
+kubectl apply -f app-three-service.yaml
+
+#running all three applications
+kubectl run app-one
+kubectl run app-two
+kubectl run app-three
