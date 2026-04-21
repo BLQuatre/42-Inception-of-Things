3252ac8279e9353873378fdbd8c7770051fdc003
Clems <79931992+BLQuatre@users.noreply.github.com>
2026-04-21 12:47:01 +0200

    feat: Update provisioning scripts to correctly reference server and worker configurations, and add node-token handling

--- p1/scripts/s_install-dependencies.sh
+++ p1/scripts/s_install-dependencies.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 export K3S_KUBECONFIG_MODE="644"
-export K3S_TOKEN="vagrant-k3s-secret-token-12345"
 export INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --flannel-iface=eth1"
+
+TOKEN_FILE="/var/lib/rancher/k3s/server/node-token"
 
 # Install K3s in server (controller) mode
 wget -qO- https://get.k3s.io | sh -s -
+
+while [ ! -f $TOKEN_FILE ]; do
+	echo "Waiting for k3s token..."
+	sleep 2
+done
+
+cat $TOKEN_FILE > /vagrant/node-token
--- p1/scripts/sw_install-dependencies.sh
+++ p1/scripts/sw_install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export K3S_URL="https://192.168.56.110:6443"
-export K3S_TOKEN="vagrant-k3s-secret-token-12345"
+export K3S_TOKEN=$(cat /vagrant/node-token)
 export INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --flannel-iface=eth1"
 
 # Install K3s in agent mode
