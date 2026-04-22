9a9b8a1d012f6c10ef4b56f9d6a60c8b3bc7c325
MiniKlar <omontloris@gmail.com>
2026-04-23 00:01:37 +0200

    feat: Update Vagrantfile and scripts for improved VM configuration and application deployment

---
+++ p2/scripts/change-html.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# On boucle sur tous les pods de chaque application pour modifier leur index.html
+# en y insérant le nom du pod. Cela permet de voir quel replica répond.
+
+# Application 1
+for pod in $(sudo kubectl get pods -l app=app-one -o name); do
+    sudo kubectl exec $pod -- sh -c "echo '<h1>Hello from POD: $pod</h1>' > /usr/share/nginx/html/index.html"
+done
+
+# Application 2
+for pod in $(sudo kubectl get pods -l app=app-two -o name); do
+    sudo kubectl exec $pod -- sh -c "echo '<h1>Hello from POD: $pod</h1>' > /usr/share/nginx/html/index.html"
+done
+
+# Application 3
+for pod in $(sudo kubectl get pods -l app=app-three -o name); do
+    sudo kubectl exec $pod -- sh -c "echo '<h1>Hello from POD: $pod</h1>' > /usr/share/nginx/html/index.html"
+done
--- p2/scripts/install-dependencies.sh
+++ p2/scripts/install-dependencies.sh
@@ -3,24 +3,25 @@
 #install curl
 apk add curl
 
-
-
 #install k3s
 curl -sfL https://get.k3s.io | sh -
 
+#move to the correct folder
+cd /mnt
+
 #Applying app one configuration
-kubectl apply -f app-one-deployment.yaml
-kubectl apply -f app-one-service.yaml
+sudo kubectl apply -f app-one-deployment.yaml
+sudo kubectl apply -f app-one-service.yaml
 
 #Applying app two configuration
-kubectl apply -f app-two-deployment.yaml
-kubectl apply -f app-two-service.yaml
+sudo kubectl apply -f app-two-deployment.yaml
+sudo kubectl apply -f app-two-service.yaml
 
 #Applying app three configuration
-kubectl apply -f app-three-deployment.yaml
-kubectl apply -f app-three-service.yaml
+sudo kubectl apply -f app-three-deployment.yaml
+sudo kubectl apply -f app-three-service.yaml
 
-#running all three applications
-kubectl run app-one
-kubectl run app-two
-kubectl run app-three
+# #running all three applications
+# sudo kubectl run app-one
+# sudo kubectl run app-two
+# sudo kubectl run app-three
