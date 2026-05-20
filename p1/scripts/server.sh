87853c71f3c4737c9fe0a0a83a16dadcb9a105b6
Clems <79931992+BLQuatre@users.noreply.github.com>
2026-05-20 14:58:56 +0200

    fix: Update Vagrantfile and provisioning scripts for server and worker configurations

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
 
 export K3S_KUBECONFIG_MODE="644"
-export INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --flannel-iface=eth1"
+export INSTALL_K3S_EXEC="server --node-external-ip=192.168.56.110 --bind-address=192.168.56.110 --flannel-iface=eth1"
+wget -qO- https://get.k3s.io | sh -
 
-TOKEN_FILE="/var/lib/rancher/k3s/server/node-token"
-
-# Install K3s in server (controller) mode
-wget -qO- https://get.k3s.io | sh -s -
-
-while [ ! -f "$TOKEN_FILE" ]; do
-	echo "Waiting for k3s token..."
-	sleep 2
+while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
+	sleep 1
+	echo "Waiting for token file..."
 done
 
-cp "$TOKEN_FILE" /vagrant/node-token
-
-while [ ! -f /etc/rancher/k3s/k3s.yaml ]; do
-	echo "Waiting for k3s kubeconfig..."
-	sleep 2
-done
-
-sed 's/127.0.0.1/192.168.56.110/g' /etc/rancher/k3s/k3s.yaml > /vagrant/k3s.yaml
+cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
+export K3S_TOKEN_FILE="/vagrant/node-token"
 export K3S_URL="https://192.168.56.110:6443"
-export K3S_TOKEN=$(cat /vagrant/node-token)
-export INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --flannel-iface=eth1"
+export INSTALL_K3S_EXEC="--flannel-iface=eth1"
+wget -qO- https://get.k3s.io | sh -
 
-# Install K3s in agent mode
-wget -qO- https://get.k3s.io | sh -s -
-
-mkdir -p /root/.kube
-cp /vagrant/k3s.yaml /root/.kube/config
-
-rm /vagrant/node-token
-rm /vagrant/k3s.yaml
+rm -f /vagrant/node-token
